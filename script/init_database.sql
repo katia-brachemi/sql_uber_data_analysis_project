44bc149c2a492c10fe3455791344b2e922a787c5
Katia Ourida Brachemi <katiabrachemi42@gmail.com>
2026-01-24 11:54:15 +0100

    Create UBER database initialization script
    This script initializes a new database named 'UBER' in the master database context.

---
+++ script/init_database.sql
@@ -0,0 +1,24 @@
+/*
+===========================================================
+ Script: UBER Setup
+===========================================================
+
+Purpose:
+ - Create a new database named 'UBER'
+  
+ Notes:
+   - Run this script in the 'master' database context
+   - Each GO statement marks the end of a batch
+===========================================================
+*/
+
+USE master;
+GO
+
+CREATE DATABASE UBER;
+GO
+
+USE UBER;
+GO
+
+-- Database UBER successfully created 
